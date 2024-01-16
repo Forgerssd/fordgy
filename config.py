dd7f28f3f4f8a01b1ee8f26c60f752b003be83a4
Forgerssd <118015300+Forgerssd@users.noreply.github.com>
2024-01-16 19:42:14 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 settings = {
-    'token': 'MTE4NjgwNDc3MjA0MTM5NjIyNg.GA3hbN.2R36UhS3eABnM4thx6oL0WLZuT9rdUKjfHGkHo',
+    'token': 'Token',
     'bot': 'Fordgy',
     'id': 1186804772041396226,
     'prefix': '-',
